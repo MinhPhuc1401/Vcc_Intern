25306917167897bc326a533b18efef88effc67b8
MinhPhuc1401 <125237808+MinhPhuc1401@users.noreply.github.com>
2023-10-29 23:58:06 +0700

    Update hashCode-equals.java

--- Week2/src/hashCode-equals.java
+++ Week2/src/hashCode-equals.java
@@ -1,1 +1,22 @@
+public class Person {
+    private String name;
+    private int age;
 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+}

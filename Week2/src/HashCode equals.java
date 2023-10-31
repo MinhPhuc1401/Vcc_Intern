a6bc0819cd9e6a52650304efa989bcdd9ab94759
MinhPhuc1401 <125237808+MinhPhuc1401@users.noreply.github.com>
2023-10-31 21:01:01 +0700

    Update and rename hashCode-equals.java to HashCode equals.java

---
+++ Week2/src/HashCode equals.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        // Tạo một HashSet chứa các đối tượng Person
+        Set<Person> personSet = new HashSet<>();
+
+        // Thêm các đối tượng Person vào HashSet
+        Person person1 = new Person("Alice", 30);
+        Person person2 = new Person("Bob", 25);
+        Person person3 = new Person("Alice", 30); // Trùng lặp với person1
+
+        personSet.add(person1);
+        personSet.add(person2);
+        personSet.add(person3); // Không được thêm vào vì đã tồn tại person1 có cùng giá trị
+
+        // In ra các phần tử trong HashSet
+        for (Person person : personSet) {
+            System.out.println("Tên: " + person.getName() + ", Tuổi: " + person.getAge());
+        }
+    }
+}

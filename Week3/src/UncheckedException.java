0ede43ce52880ac083e6f8bb8bc2b2459c26ada8
MinhPhuc1401 <125237808+MinhPhuc1401@users.noreply.github.com>
2023-11-06 00:13:48 +0700

    Update UncheckedException.java

--- Week3/src/UncheckedException.java
+++ Week3/src/UncheckedException.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+class UncheckedException {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try {
+            int numbers[] = {10, 20, 30};
+            int index = input.nextInt();
+            System.out.println(numbers[index]);
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+        input.close();
+    }
+}
 

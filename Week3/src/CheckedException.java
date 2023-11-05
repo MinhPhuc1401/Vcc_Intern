468e1894e0db93616ed8086dc0d09e542aaa66d4
MinhPhuc1401 <125237808+MinhPhuc1401@users.noreply.github.com>
2023-11-06 00:13:23 +0700

    Update CheckedException.java

--- Week3/src/CheckedException.java
+++ Week3/src/CheckedException.java
@@ -1,1 +1,13 @@
+import java.io.File;
+import java.io.FileInputStream;
 
+public class CheckedException  {
+    public static void main(String[] args) {
+      try{
+          File file = new File("not_exist.txt");
+          FileInputStream stream = new FileInputStream(file);
+      } catch (Exception e){
+          throw new RuntimeException("The file does not exist");
+      }
+    }
+}

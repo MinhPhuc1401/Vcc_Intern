28042967cc5f13e30638b4890c005b8889128407
MinhPhuc1401 <125237808+MinhPhuc1401@users.noreply.github.com>
2023-11-06 02:11:31 +0700

    Update JSONObject to String.java

--- Week3/src/JSONObject to String.java
+++ Week3/src/JSONObject to String.java
@@ -1,1 +1,13 @@
+import org.json.JSONObject;
 
+public class JSONExample {
+    public static void main(String[] args) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "Phuc");
+        jsonObject.put("age", 21);
+        jsonObject.put("city", "Hai Duong");
+
+        String jsonString = jsonObject.toString();
+        System.out.println("JSON String: " + jsonString);
+    }
+}
